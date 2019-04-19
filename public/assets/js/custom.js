a9bd7e78b5c050c0a2cfcffc4327b74b1585a34e
DESKTOP-U1PUFMN\hp <ouksoo@nate.com>
2019-04-19 14:31:45 +0900

    Chart First commit

---
+++ public/assets/js/custom.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var LIM = {
+    init: function init() {
+        console.log('lim init');
+    }
+
+    // after loaded execute
+};window.onload = function () {
+    LIM.init();
+};
---
+++ src/js/custom.js
@@ -0,0 +1,11 @@
+let LIM = {
+    init: function() {
+        console.log('lim init');
+    }
+}
+
+// after loaded execute
+window.onload = function() {
+    LIM.init(); 
+
+};
