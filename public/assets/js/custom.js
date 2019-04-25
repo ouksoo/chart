ce2b3a18374c1773443c453f0526604be092f1cc
DESKTOP-U1PUFMN\hp <ouksoo@nate.com>
2019-04-25 14:22:21 +0900

    -  인트로 로그인 진행중

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -1,11 +1,47 @@
 'use strict';
 
-var LIM = {
+var PJT = {
     init: function init() {
-        console.log('lim init');
+        this.introAnimationStart();
+    },
+    introAnimationStart: function introAnimationStart() {
+        var sequenceStart = void 0;
+        var position = 250;
+        var interval = 25;
+        var diff = 250;
+        var sequencesize = 2500;
+        sequenceStart = setInterval(function () {
+            document.getElementById('sequenceArea').style.backgroundPosition = '-' + position + 'px 0px';
+            if (position < sequencesize) {
+                position = position + diff;
+            } else {
+                position = 250;
+                clearInterval(sequenceStart);
+                $('#sequenceArea').on('click', function () {
+                    PJT.loadImageRandom();
+                    $('.intro-bg').fadeOut('slow');
+                });
+            }
+        }, interval);
+    },
+    loadImageRandom: function loadImageRandom() {
+        var imageClass = ['type-1', 'type-2', 'type-3'];
+        $('.image-bg').addClass(imageClass[Math.floor(Math.random() * imageClass.length)]).addClass('on');
+        $('.login-wrap').fadeIn('slow', function () {
+            $('header, footer').addClass('on');
+            PJT.loginKeyCheck();
+        });
+    },
+    loginKeyCheck: function loginKeyCheck() {
+        $('.login-wrap input').on('keydown', function (e) {
+            if ($('#userid').val() !== '' && $('#passwd').val() !== '') {
+                $('.login-wrap a.login-button').fadeIn('slow');
+            }
+        });
     }
 
     // after loaded execute
 };window.onload = function () {
-    LIM.init();
+    PJT.init();
+    PJT.loginKeyCheck();
 };
--- src/js/custom.js
+++ src/js/custom.js
@@ -1,11 +1,48 @@
-let LIM = {
+let PJT = {
     init: function() {
-        console.log('lim init');
+        this.introAnimationStart();
+    },
+    introAnimationStart: function() {
+        let sequenceStart;
+        let position = 250;
+        const interval = 25;
+        const diff = 250;
+        const sequencesize = 2500;
+        sequenceStart = setInterval(() => {
+            document.getElementById('sequenceArea').style.backgroundPosition = `-${position}px 0px`;
+            if(position < sequencesize) {
+                position = position + diff;
+            }
+            else {
+                position = 250;
+                clearInterval(sequenceStart);
+                $('#sequenceArea').on('click', function() {
+                    PJT.loadImageRandom();
+                    $('.intro-bg').fadeOut('slow');
+                });
+            }
+        }, interval);
+    },
+    loadImageRandom: function() {
+        var imageClass = ['type-1', 'type-2', 'type-3'];
+        $('.image-bg').addClass(imageClass[Math.floor(Math.random() * imageClass.length)]).addClass('on');
+        $('.login-wrap').fadeIn('slow', function() {
+            $('header, footer').addClass('on');
+            PJT.loginKeyCheck();
+        });
+    },
+    loginKeyCheck: function() {
+        $('.login-wrap input').on('keydown', function(e) {
+            if($('#userid').val() !== '' && $('#passwd').val() !== '') {
+                $('.login-wrap a.login-button').fadeIn('slow');
+            }
+        });
     }
 }
 
+
 // after loaded execute
 window.onload = function() {
-    LIM.init(); 
-
+    PJT.init(); 
+    PJT.loginKeyCheck();
 };
